22082475d2a2355b66f01a7bef899838518c6010
Micaela <micarancibia94@gmail.com>
2023-08-21 10:54:19 -0300

    borrando alguno comments

--- src/drawer/Drawer.jsx
+++ src/drawer/Drawer.jsx
@@ -1,17 +1,14 @@
-import React from 'react';
-import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import { Link } from 'react-router-dom';
+import React from "react";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import { Link } from "react-router-dom";
 
 const CustomDrawer = ({ open, onClose }) => {
   return (
-    // Renderiza el componente Drawer de Material-UI
     <Drawer anchor="left" open={open} onClose={onClose}>
-      {/* Lista de elementos en el Drawer */}
       <List>
-        {/* Cada elemento es un ListItem que enlaza a diferentes rutas */}
         <ListItem button component={Link} to="/">
           <ListItemText primary="Inicio" />
         </ListItem>
--- src/popUpMenu/PopUpMenu.jsx
+++ src/popUpMenu/PopUpMenu.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import { Link } from 'react-router-dom';
+import React from "react";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { Link } from "react-router-dom";
 
 const CustomPopMenu = ({ anchorEl, onClose }) => {
   return (
-    // Renderiza el componente Menu de Material-UI
     <Menu
       anchorEl={anchorEl}
       anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       keepMounted
       open={Boolean(anchorEl)}
       onClose={onClose}
     >
-      {/* Cada elemento del menú es un MenuItem con acciones */}
       <MenuItem onClick={onClose}>Profile</MenuItem>
-      <MenuItem button component={Link} to="/" onClick={onClose}>Inicio</MenuItem>
+      <MenuItem button component={Link} to="/" onClick={onClose}>
+        Inicio
+      </MenuItem>
     </Menu>
   );
 };
